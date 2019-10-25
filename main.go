fe58c0287b9f25d9a0e569e2b0b147ee1246007d
Federico Ragona <federico.ragona@gmail.com>
2019-10-25 22:52:28 +0200

    POST /block

--- main.go
+++ main.go
@@ -3,38 +3,76 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 )
 
 var ledger *Ledger
+var unverified chan Block
+
+// BlockRequest describes the POST body of a request to create a new block
+type BlockRequest struct {
+	Data Data `json:"data"`
+}
+
+func blockHandler(w http.ResponseWriter, r *http.Request) {
+	blockReq := &BlockRequest{}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(body, blockReq)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	block, err := Create(ledger, blockReq.Data)
+	if err != nil {
+		log.Println("Unable to create block")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	unverified <- *block
+}
 
 func ledgerHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := json.Marshal(ledger)
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(ledger)
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusOK)
-		w.Write(body)
 	}
 }
 
 func serve() {
 	router := mux.NewRouter()
 	router.HandleFunc("/ledger", ledgerHandler).Methods("GET")
+	router.HandleFunc("/block", blockHandler).Methods("POST")
 
 	http.ListenAndServe(":9999", router)
 }
 
+func append(verified <-chan Block) {
+	for b := range verified {
+		block := b
+		ledger.Append(&block)
+		fmt.Println("Appended block", block.Hash, "to the ledger")
+	}
+}
+
 func main() {
 	ledger = NewLedger("We ❤️ blockchains")
+	unverified = make(chan Block)
 
 	updates := make(chan Ledger)
-	unverified := make(chan Block)
 	verified := make(chan Block)
 	node := &Node{
 		Updates:    updates,
@@ -43,27 +81,9 @@ func main() {
 	}
 
 	go node.Run()
-
 	updates <- *ledger
 
-	go func() {
-		t := time.NewTicker(time.Second * 5)
-
-		for {
-			select {
-			case <-t.C:
-				block, err := Create(ledger, "foobar")
-				if err != nil {
-					log.Println("Unable to create block")
-				}
-
-				unverified <- *block
-			case block := <-verified:
-				ledger.Append(&block)
-				fmt.Println("Appended block", block.Hash, "to the ledger")
-			}
-		}
-	}()
+	go append(verified)
 
 	serve()
 }
